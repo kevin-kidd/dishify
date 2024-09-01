af10764d87283bf35ae15aeb1ac0346eb60c21d9
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-01 16:10:33 -0400

    feat(next): integrate Sentry for error monitoring and performance tracing

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,31 +1,32 @@
-const million = require("million/compiler")
-const { withExpo } = require("@expo/next-adapter")
+const million = require("million/compiler");
+const { withExpo } = require("@expo/next-adapter");
+const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
-})
+});
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   sw: "service-worker.js",
   swcMinify: true,
-})
+});
 
 const boolVals = {
   true: true,
   false: false,
-}
+};
 
 const disableBrowserLogs =
-  boolVals[process.env.DISABLE_BROWSER_LOGS] ?? process.env.NODE_ENV === "production"
+  boolVals[process.env.DISABLE_BROWSER_LOGS] ?? process.env.NODE_ENV === "production";
 
 const enableMillionJS =
-  boolVals[process.env.ENABLE_MILLION_JS] ?? process.env.NODE_ENV === "production"
+  boolVals[process.env.ENABLE_MILLION_JS] ?? process.env.NODE_ENV === "production";
 
 // Enabling might cause FOUC on page refreshes
-const optimizeCss = true
+const optimizeCss = true;
 
-const plugins = [withPWA, withExpo, withBundleAnalyzer]
+const plugins = [withPWA, withExpo, withBundleAnalyzer];
 
 module.exports = () => {
   /** @type {import('next').NextConfig} */
@@ -70,22 +71,64 @@ module.exports = () => {
     compiler: {
       removeConsole: disableBrowserLogs,
     },
-  }
+  };
 
   for (const plugin of plugins) {
     config = {
       ...config,
       ...plugin(config),
-    }
+    };
   }
 
   const millionConfig = {
     auto: true,
     mute: true,
-  }
+  };
 
   if (enableMillionJS) {
-    return million.next(config, millionConfig)
+    return million.next(config, millionConfig);
   }
-  return config
-}
+  return config;
+};
+
+// Injected content via Sentry wizard below
+
+module.exports = withSentryConfig(module.exports, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: "dishify",
+  project: "dishify-web",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
