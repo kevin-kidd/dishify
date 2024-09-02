288d5eebdbf8055a5c7950ffee9090d821b1e172
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-01 21:25:30 -0400

    refactor(ui): clean up comment formatting in layout.tsx

--- packages/ui/src/elements/layout.tsx
+++ packages/ui/src/elements/layout.tsx
@@ -1,9 +1,7 @@
 import { View } from "react-native";
 import { withWebTag } from "../utils/web-tag";
 
-{
-  /* Lists */
-}
+/* Lists */
 export const UnorderedList = withWebTag(View, "ul", "list-disc list-inside");
 export const OrderedList = withWebTag(View, "ol", "list-decimal list-inside");
 export const ListItem = withWebTag(View, "li");
