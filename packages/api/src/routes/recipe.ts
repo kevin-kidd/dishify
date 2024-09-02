a7db116f50c701dda34995167075daaae635aa63
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-01 21:25:10 -0400

    refactor(api): remove unused recipe routes

--- packages/api/src/routes/recipe.ts
+++
@@ -1,7 +0,0 @@
-import { generate } from "./recipe/generate";
-import { autocomplete } from "./recipe/autocomplete";
-import { router } from "../trpc";
-export const recipeRouter = router({
-  generate: generate,
-  autocomplete: autocomplete,
-});
