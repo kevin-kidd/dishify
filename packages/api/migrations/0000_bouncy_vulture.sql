b6f634a10f68f02f7f718e42e0edb0c6ee4c883c
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-02 21:16:26 -0400

    refactor(migrations): delete initial migration and rename subsequent migration files

--- packages/api/migrations/0000_bouncy_vulture.sql
+++ packages/api/migrations/0000_bouncy_vulture.sql
@@ -1,21 +1,26 @@
 CREATE TABLE `english_recipes` (
-	`name` text NOT NULL
+	`name` text PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `french_recipes` (
-	`name` text NOT NULL
+	`name` text PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `german_recipes` (
-	`name` text NOT NULL
+	`name` text PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `italian_recipes` (
-	`name` text NOT NULL
+	`name` text PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `spanish_recipes` (
-	`name` text NOT NULL
+	`name` text PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `User` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text NOT NULL
 );
 --> statement-breakpoint
 CREATE INDEX `name_idx` ON `english_recipes` (`name`);
