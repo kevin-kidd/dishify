1e048e69b26e4b1317f742adc97cfd492c18c166
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-09 21:41:33 -0400

    feat(ui): enhance layout elements with Expo HTML elements and new section components

--- packages/ui/src/elements/layout.tsx
+++ packages/ui/src/elements/layout.tsx
@@ -1,7 +1,52 @@
 import { View } from "react-native";
 import { withWebTag } from "../utils/web-tag";
+import { UL as ExpoUL, LI as ExpoLI } from "@expo/html-elements";
+import type { ViewProps } from "@expo/html-elements/build/primitives/View";
+import { cn } from "../utils";
+import {
+  Section as ExpoSection,
+  Main as ExpoMain,
+  Div as ExpoDiv,
+  Header as ExpoHeader,
+  Footer as ExpoFooter,
+  Nav as ExpoNav,
+} from "@expo/html-elements";
 
 /* Lists */
-export const UnorderedList = withWebTag(View, "ul", "list-disc list-inside");
-export const OrderedList = withWebTag(View, "ol", "list-decimal list-inside");
-export const ListItem = withWebTag(View, "li");
+export const UL = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoUL className={cn("list-disc list-inside", className)} {...rest}>
+    {children}
+  </ExpoUL>
+);
+export const LI = ExpoLI;
+export const OL = withWebTag(View, "ol", "list-decimal list-inside");
+
+/* Sections */
+export const Section = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoSection className={cn("flex flex-col gap-6", className)} {...rest}>
+    {children}
+  </ExpoSection>
+);
+export const Main = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoMain className={cn("flex flex-col gap-6", className)} {...rest}>
+    {children}
+  </ExpoMain>
+);
+export const Header = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoHeader className={cn("flex flex-col gap-6", className)} {...rest}>
+    {children}
+  </ExpoHeader>
+);
+export const Footer = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoFooter className={cn("flex flex-col gap-6", className)} {...rest}>
+    {children}
+  </ExpoFooter>
+);
+export const Nav = ({ className, children, ...rest }: ViewProps) => (
+  <ExpoNav className={cn("flex flex-col gap-6", className)} {...rest}>
+    {children}
+  </ExpoNav>
+);
+
+/* Containers */
+export const Div = ({ children, ...rest }: ViewProps) => <ExpoDiv {...rest}>{children}</ExpoDiv>;
