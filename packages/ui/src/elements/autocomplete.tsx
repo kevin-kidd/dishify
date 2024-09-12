2a0768bebbb3b0d15fe6b3be022b7c5ce35ccdda
Kevin Kidd <harrison.kevin@protonmail.com>
2024-09-11 23:18:37 -0400

    refactor(ui/autocomplete): refactor autocomplete component to use new useAutocomplete hook

--- packages/ui/src/elements/autocomplete.tsx
+++ packages/ui/src/elements/autocomplete.tsx
@@ -1,16 +1,13 @@
 import { isWeb } from "@tamagui/constants";
-import React, { useCallback, useRef, useState, useEffect } from "react";
-import { debounce } from "lodash";
-import type { PopoverTriggerRef } from "@rn-primitives/popover";
+import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Div } from "./layout";
 import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 import { cn } from "../utils";
-import { Pressable, findNodeHandle } from "react-native";
-import type { KeyboardEvent } from "react";
-import type { NativeSyntheticEvent, TextInputKeyPressEventData } from "react-native";
+import { Pressable } from "react-native";
 import type { TextInput } from "./input";
 import { Text } from "./text";
+import { useAutocomplete } from "../utils/hooks/autocomplete";
 
 interface AutocompleteProps {
   children: React.ReactElement;
@@ -27,12 +24,19 @@ export function Autocomplete({
   autocompleteOptions,
   className,
 }: AutocompleteProps) {
-  const blurTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const triggerRef = useRef<PopoverTriggerRef>(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const inputRef = useRef<React.ElementRef<typeof TextInput>>(null);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [inputValue, setInputValue] = useState("");
+  const {
+    triggerRef,
+    inputRef,
+    isOpen,
+    setIsOpen,
+    inputValue,
+    selectedIndex,
+    handleInputChange,
+    handleSelectOption,
+    handleFocus,
+    handleInteractOutside,
+    handleKeyPress,
+  } = useAutocomplete({ onSelect, getOptions, autocompleteOptions, children });
 
   const insets = useSafeAreaInsets();
   const contentInsets = {
@@ -41,114 +45,6 @@ export function Autocomplete({
     left: 12,
     right: 12,
   };
-
-  const resetSelectedIndex = useCallback(() => setSelectedIndex(-1), []);
-
-  const debouncedSearch = useCallback(
-    debounce(async (text: string) => {
-      if (text.length >= 3) {
-        // Only fetch options if the user has typed at least 3 characters
-        await getOptions();
-        resetSelectedIndex();
-      } else {
-        triggerRef.current?.close?.();
-      }
-    }, 300),
-    []
-  );
-
-  const handleInputChange = useCallback(
-    (text: string) => {
-      debouncedSearch(text);
-      if (isWeb) {
-        children.props.onChange?.(text);
-      } else {
-        children.props.onChangeText?.(text);
-      }
-      setInputValue(text);
-      if (!isOpen) {
-        triggerRef.current?.open?.();
-      }
-    },
-    [debouncedSearch, children.props, isOpen]
-  );
-
-  const handleSelectOption = useCallback(
-    (option: string) => {
-      onSelect(option);
-      handleInputChange(option);
-      triggerRef.current?.close?.();
-    },
-    [onSelect, handleInputChange]
-  );
-
-  const handleFocus = useCallback(() => {
-    if (blurTimeoutRef.current) {
-      clearTimeout(blurTimeoutRef.current);
-      blurTimeoutRef.current = null;
-    }
-    resetSelectedIndex();
-    triggerRef.current?.open?.();
-    children.props.onFocus?.();
-  }, [children.props.onFocus, resetSelectedIndex]);
-
-  useEffect(() => {
-    return () => {
-      if (blurTimeoutRef.current) {
-        clearTimeout(blurTimeoutRef.current);
-      }
-    };
-  }, []);
-
-  const handleInteractOutside = useCallback((e: any) => {
-    if (isWeb) {
-      // Web-specific check
-      const inputElement = inputRef.current as unknown as HTMLInputElement;
-      if (inputElement && (e.target === inputElement || inputElement.contains(e.target as Node))) {
-        return; // Do nothing if the interaction is within the input
-      }
-    } else {
-      // React Native-specific check
-      const inputHandle = findNodeHandle(inputRef.current);
-      if (inputHandle && e.target === inputHandle) {
-        return; // Do nothing if the interaction is with the input
-      }
-    }
-    e.preventDefault();
-    triggerRef.current?.close?.();
-  }, []);
-
-  const handleKeyPress = useCallback(
-    (event: KeyboardEvent<HTMLInputElement> | NativeSyntheticEvent<TextInputKeyPressEventData>) => {
-      if (!isOpen || !autocompleteOptions || autocompleteOptions.length === 0) return;
-
-      const key = isWeb
-        ? (event as KeyboardEvent<HTMLInputElement>).key
-        : (event as NativeSyntheticEvent<TextInputKeyPressEventData>).nativeEvent.key;
-
-      switch (key) {
-        case "ArrowDown":
-          event.preventDefault();
-          setSelectedIndex((prevIndex) =>
-            prevIndex < autocompleteOptions.length - 1 ? prevIndex + 1 : 0
-          );
-          break;
-        case "ArrowUp":
-          event.preventDefault();
-          setSelectedIndex((prevIndex) =>
-            prevIndex > 0 ? prevIndex - 1 : autocompleteOptions.length - 1
-          );
-          break;
-        case "Enter":
-          if (selectedIndex !== -1) {
-            event.preventDefault();
-            handleSelectOption(autocompleteOptions[selectedIndex]);
-          }
-          break;
-      }
-    },
-    [isOpen, autocompleteOptions, selectedIndex, handleSelectOption]
-  );
 
   const inputWithAutocomplete = React.cloneElement<React.ComponentProps<typeof TextInput>>(
     children,
@@ -172,8 +68,6 @@ export function Autocomplete({
       )),
     [autocompleteOptions, handleSelectOption, selectedIndex]
   );
-
-  // TODO: add keyboard navigation
 
   return (
     <Div className={cn(className, "w-full")}>
