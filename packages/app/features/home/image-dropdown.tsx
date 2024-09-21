687eab25f353fc36889ab022bf2471e3cd2292a7
Kevin Kidd <harrison.kevin@pm.me>
2024-09-21 04:15:57 -0400

    feat(home): add image upload and preview functionality in dropdown menu

---
+++ packages/app/features/home/image-dropdown.tsx
@@ -0,0 +1,139 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@dishify/ui";
+import { ImageIcon } from "@dishify/ui/src/icons/image";
+import { CameraIcon } from "@dishify/ui/src/icons/camera";
+import { UploadIcon } from "lucide-react-native";
+import { Button, Text } from "@dishify/ui/src";
+import { isWeb } from "@tamagui/constants";
+import * as ImagePicker from "expo-image-picker";
+import { type BaseSyntheticEvent, useRef, useState } from "react";
+import ImagePreview from "./image-preview";
+import type { SearchValues } from "@dishify/api/schemas/search";
+import type { UseFormWatch } from "react-hook-form";
+import { toast } from "app/utils/toast";
+import { resizeImage } from "app/utils/image";
+
+interface ImageDropdownProps {
+  setImageData: (imageData: number[] | undefined) => void;
+  watch: UseFormWatch<SearchValues>;
+  onSubmit: (e?: BaseSyntheticEvent) => Promise<void>;
+}
+
+export default function ImageDropdown({ setImageData, watch, onSubmit }: ImageDropdownProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleUpload = async () => {
+    setImageData(undefined);
+    if (isWeb) {
+      // Reset the file input value
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+      // Trigger file input click
+      fileInputRef.current?.click();
+    } else {
+      try {
+        // React Native upload
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+
+        if (!result.canceled && result.assets[0]?.uri) {
+          const response = await fetch(result.assets[0].uri);
+          const blob = await response.blob();
+          processFile(blob);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      processFile(file);
+    }
+  };
+
+  const processFile = (file: Blob) => {
+    const maxWidth = 800;
+    const maxSizeInBytes = 1024 * 1024; // 1MB
+    const promise = new Promise<void>((resolve, reject) => {
+      const processAndResize = async () => {
+        try {
+          let resizedImage = await resizeImage(file, maxWidth, maxSizeInBytes);
+
+          if (resizedImage.length > maxSizeInBytes) {
+            // If still too large, try resizing to 400px width
+            resizedImage = await resizeImage(file, 400, maxSizeInBytes);
+
+            if (resizedImage.length > maxSizeInBytes) {
+              throw new Error("Image size is too large");
+            }
+          }
+
+          setImageData(Array.from(resizedImage));
+          setIsDialogOpen(true);
+          resolve();
+        } catch (error) {
+          console.error("Error processing file:", error);
+          reject(error);
+        }
+      };
+
+      processAndResize();
+    });
+
+    const id = toast.promise(promise, {
+      loading: "Processing image...",
+      success: () => toast.dismiss(id),
+      error: "Error processing image",
+    });
+  };
+
+  const handleTakePhoto = () => {
+    console.log("Take photo");
+  };
+  return (
+    <>
+      {isWeb && (
+        <input type="file" ref={fileInputRef} onChange={handleFileChange} hidden accept="image/*" />
+      )}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon">
+            <ImageIcon className="text-primary-foreground p-0.5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuItem onPress={handleUpload}>
+              <UploadIcon className="text-primary-foreground p-0.5" />
+              <Text>Upload</Text>
+            </DropdownMenuItem>
+            <DropdownMenuItem onPress={handleTakePhoto}>
+              <CameraIcon className="text-primary-foreground p-0.5" />
+              <Text>Take photo</Text>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ImagePreview
+        watch={watch}
+        setImageData={setImageData}
+        isOpen={isDialogOpen}
+        onOpenChange={setIsDialogOpen}
+        onSubmit={onSubmit}
+      />
+    </>
+  );
+}
