21ff4e272ad49200e073b5b8431272e0a0c0ce98
Kevin Kidd <harrison.kevin@pm.me>
2024-09-21 04:14:54 -0400

    feat(ui): add style prop to Image component

--- packages/ui/src/elements/image.tsx
+++ packages/ui/src/elements/image.tsx
@@ -3,19 +3,21 @@ import { SolitoImage } from "solito/image";
 import { View } from "react-native";
 import type { ImageProps as NextImageProps } from "next/image";
 import type { ImageProps as RNImageProps } from "react-native";
+import type { ImageStyle } from "react-native";
 
 type CommonImageProps = {
   className?: string;
   src: NextImageProps["src"] | RNImageProps["source"];
   alt?: string;
+  style?: ImageStyle;
 };
 
 type ImageProps = CommonImageProps & Omit<ComponentProps<typeof SolitoImage>, "src">;
 
-export function Image({ className, src, alt, fill = false, ...rest }: ImageProps) {
+export function Image({ className, src, alt, style, fill = false, ...rest }: ImageProps) {
   return (
     <View className={className}>
-      <SolitoImage {...rest} src={src as any} alt={alt} fill={fill} />
+      <SolitoImage {...rest} src={src as any} alt={alt} fill={fill} style={style} />
     </View>
   );
 }
