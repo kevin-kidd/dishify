acf622268bc7bfef7ff0e58c1d4eaafc75442fe8
Kevin Kidd <harrison.kevin@pm.me>
2025-01-25 22:51:41 -0500

    refactor(provider): remove Header component from Provider structure

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -23,10 +23,7 @@ export function Provider({
         {/* <AuthProvider initialSession={initialSession}> */}
         <TRPCProvider>
           <ThemeProvider>
-            <ToastProvider>
-              <Header />
-              {children}
-            </ToastProvider>
+            <ToastProvider>{children}</ToastProvider>
             <PortalHost />
           </ThemeProvider>
         </TRPCProvider>
