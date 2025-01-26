6da955ef25fc158917f75f665999b98aa62f1386
Kevin Kidd <harrison.kevin@pm.me>
2025-01-25 22:52:05 -0500

    feat(home): add trending component for native app

---
+++ packages/app/features/home/trending/index.native.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@dishify/ui/src";
+import { View } from "react-native";
+
+export default function Trending() {
+  return (
+    <View>
+      <Text>Trending</Text>
+    </View>
+  );
+}
