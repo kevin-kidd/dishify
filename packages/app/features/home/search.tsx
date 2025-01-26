89c87340eb28e36b1b665f4e793740d9c975cd01
Kevin Kidd <harrison.kevin@pm.me>
2025-01-25 22:51:49 -0500

    refactor(search): update search component layout and input handling

--- packages/app/features/home/search.tsx
+++ packages/app/features/home/search.tsx
@@ -1,6 +1,6 @@
 import { trpc } from "app/utils/trpc";
-import { useCallback, useRef } from "react";
-import { Controller, useForm, useWatch } from "react-hook-form";
+import { useCallback, useRef, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SearchSchema, type SearchValues } from "@dishify/api/schemas/search";
 import { isWeb } from "@tamagui/constants";
@@ -9,8 +9,9 @@ import { LoaderCircle } from "@dishify/ui/src/icons/loader-circle";
 import ImageDropdown from "./image-dropdown";
 import { toast } from "app/utils/toast";
 import { Keyboard, type Pressable } from "react-native";
-import { Autocomplete, Button, Form, FormInput, TextInput } from "@dishify/ui";
+import { Autocomplete, Button, cn, Div, Form, FormInput, TextInput } from "@dishify/ui";
 import { useAtom } from "jotai";
+import { Search as SearchIcon } from "@dishify/ui/src/icons/search";
 import { recipeAtom } from "app/atoms/recipe";
 
 export default function Search() {
@@ -28,17 +29,14 @@ export default function Search() {
       image: [],
     },
   });
+  const [isFocused, setIsFocused] = useState(false);
   const [recipe, setRecipe] = useAtom(recipeAtom);
   const submitButtonRef = useRef<React.ElementRef<typeof Pressable>>(null);
   const generate = trpc.recipe.generate.useMutation();
-  const dishName = useWatch({
-    control,
-    name: "dishName",
-  });
   const { data: autocompleteOptions, refetch: refetchAutocomplete } =
     trpc.recipe.autocomplete.useQuery(
       {
-        query: dishName ?? "",
+        query: watch("dishName") ?? "",
         language: "en",
       },
       {
@@ -47,9 +45,11 @@ export default function Search() {
         meta: { showToastOnError: false },
       },
     );
+
   const getOptions = useCallback(async () => {
     await refetchAutocomplete();
   }, [refetchAutocomplete]);
+
   const onSubmitImage = handleSubmit(async (data) => {
     setRecipe((prev) => {
       prev.isLoading = true;
@@ -69,6 +69,7 @@ export default function Search() {
       prev.isLoading = false;
     });
   });
+
   const onSubmit = handleSubmit(async (data) => {
     setRecipe((prev) => {
       prev.isLoading = true;
@@ -89,51 +90,64 @@ export default function Search() {
     } catch (error) {
       toast.error(error.message);
     }
-
     setRecipe((prev) => {
       prev.isLoading = false;
     });
   });
+
   return (
-    <Form className="mb-4 flex flex-col sm:flex-row gap-2 items-center" onSubmit={onSubmit}>
-      <Controller
-        name="dishName"
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({ field: { onChange, onBlur, name, value } }) => (
-          <FormInput error={errors.dishName?.message} id={name}>
-            <Autocomplete
-              onSelect={(selectedValue) => onChange(selectedValue)}
-              getOptions={getOptions}
-              autocompleteOptions={autocompleteOptions}
+    <Controller
+      name="dishName"
+      control={control}
+      rules={{
+        required: true,
+      }}
+      render={({ field: { onChange, onBlur, name, value } }) => (
+        <Autocomplete
+          onSelect={onChange}
+          getOptions={getOptions}
+          autocompleteOptions={autocompleteOptions}
+        >
+          <Form
+            className={cn(
+              "p-1 sm:p-2",
+              "group relative flex-1 w-full items-center flex max-h-12 sm:max-h-14",
+              "rounded-2xl overflow-hidden",
+              "bg-white border border-primary/60",
+              "transition-all duration-300 ease-in-out",
+              isFocused && "ring-2 ring-primary/80 border-transparent",
+            )}
+            onSubmit={onSubmit}
+          >
+            <FormInput
+              error={errors.dishName?.message}
+              id={name}
+              className="flex-1 w-full flex-row relative items-center gap-0"
             >
+              <SearchIcon className="absolute left-2 h-5 w-5 transition-all duration-300 text-primary" />
               <TextInput
                 inputMode="search"
                 id={name}
                 value={value}
-                onBlur={onBlur}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => {
+                  setIsFocused(false);
+                  onBlur();
+                }}
                 onChange={onChange}
-                placeholder="Enter a dish name"
+                className="text-md sm:text-lgflex border-0 bg-transparent pl-10 sm:pl-12 web:focus-visible:ring-0 web:focus-visible:ring-offset-0 placeholder:text-primary/70 transition-colors duration-300"
+                placeholder="Search any dish..."
                 maxLength={80}
               />
-            </Autocomplete>
-          </FormInput>
-        )}
-      />
-      <Button size="icon" disabled={recipe.isLoading} ref={submitButtonRef} onPress={onSubmit}>
-        {recipe.isLoading ? (
-          <LoaderCircle className="animate-spin text-primary-foreground p-0.5" />
-        ) : (
-          <SendHorizontal className="text-primary-foreground p-0.5" />
-        )}
-      </Button>
-      <ImageDropdown
-        setImageData={(imageData: number[] | undefined) => setValue("image", imageData)}
-        watch={watch}
-        onSubmit={onSubmitImage}
-      />
-    </Form>
+              <ImageDropdown
+                setImageData={(imageData: number[] | undefined) => setValue("image", imageData)}
+                watch={watch}
+                onSubmit={onSubmitImage}
+              />
+            </FormInput>
+          </Form>
+        </Autocomplete>
+      )}
+    />
   );
 }
