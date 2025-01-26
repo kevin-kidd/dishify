038e0ef538a2b24ab589117044cd027bcac2dbaf
Kevin Kidd <harrison.kevin@pm.me>
2025-01-25 22:51:54 -0500

    feat(home): enhance HomeScreen layout with new styles and components

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,16 +1,32 @@
 "use client";
 
-import { ScrollView, View } from "react-native";
+import { ScrollView } from "react-native";
 import Search from "./search";
 import RecipeCard from "./recipe-card";
+import { H1, P, Section } from "@dishify/ui/src";
+import { TrendingSection } from "./trending";
 
 export function HomeScreen() {
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="bg-background p-4">
-      <View className="container mx-auto w-full space-y-4 h-full pt-20 max-w-4xl">
-        <Search />
-        <RecipeCard />
-      </View>
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        width: "100%",
+        alignItems: "center",
+        paddingHorizontal: 16,
+      }}
+      className="bg-gradient-to-b from-white to-slate-100 h-full w-full py-16"
+    >
+      <Section className="mb-12 text-center flex flex-col items-center justify-center gap-0">
+        <H1 className="animate-fade-up text-5xl font-semibold tracking-tight my-0 py-0">Dishify</H1>
+        <P className="mt-4 animate-fade-up text-lg text-gray-600 animation-delay-100">
+          Your AI-powered culinary companion
+        </P>
+      </Section>
+
+      <Search />
+      <TrendingSection />
+      <RecipeCard />
     </ScrollView>
   );
 }
