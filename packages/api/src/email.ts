9558f65f68641fba3720e13d7a6b94cd8dc3965b
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:37:21 -0500

    feat(api): implement email verification and password reset functionality

---
+++ packages/api/src/email.ts
@@ -0,0 +1,37 @@
+import type { User } from "better-auth/types";
+import { Resend } from "resend";
+import type { Env } from "./types";
+
+export const sendVerificationEmail = async (
+  { url, user }: { url: string; user: User },
+  env: Env,
+) => {
+  const resend = new Resend(env.RESEND_API_KEY);
+  try {
+    await resend.emails.send({
+      from: "auth@dishify.app",
+      to: user.email,
+      subject: "Verify your email",
+      html: `<p>Verify your email by clicking <a href="${url}">here</a></p>.`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const sendResetPasswordEmail = async (
+  { url, user }: { url: string; user: User },
+  env: Env,
+) => {
+  try {
+    const resend = new Resend(env.RESEND_API_KEY);
+    await resend.emails.send({
+      from: "auth@dishify.app",
+      to: user.email,
+      subject: "Reset your password",
+      html: `<p>Reset your password by clicking <a href="${url}">here</a></p>.`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
