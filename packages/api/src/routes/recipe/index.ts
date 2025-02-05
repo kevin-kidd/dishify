ca350d30821ac7ff860389539019e80843a62fe1
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:38:00 -0500

    feat(api): integrate recipe get and reactions routes into recipe router

--- packages/api/src/routes/recipe/index.ts
+++ packages/api/src/routes/recipe/index.ts
@@ -1,11 +1,16 @@
 import { router } from "../../trpc";
 import { autocomplete } from "./autocomplete";
 import { generate } from "./generate";
+import { getRecipe } from "./get";
+import { toggleFavorite } from "./toggle-favorite";
+import { recipeReactionsRouter } from "./reactions";
 
 export const recipeRouter = router({
   generate,
   autocomplete,
+  getRecipe,
+  toggleFavorite,
+  reactions: recipeReactionsRouter,
 });
-const test = "";
-console.log(test);
-console.log("test: ", test);
+
+export type RecipeRouter = typeof recipeRouter;
