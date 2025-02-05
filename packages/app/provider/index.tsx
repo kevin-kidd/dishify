28f882b6faade675751278580a3fa5e640be2b80
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:31:32 -0500

    refactor(provider): remove commented auth provider and simplify provider setup

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,33 +1,27 @@
 "use client";
 
-import { TRPCProvider } from "./trpc";
+import { TRPCProvider } from "./trpc/index.web";
 import { SafeAreaProvider } from "./safe-area";
 import { SolitoImageProvider } from "./solito-image";
-import type { Session } from "@supabase/supabase-js";
-import { AuthProvider } from "./auth";
 import { ThemeProvider } from "./theme";
 import { PortalHost } from "@rn-primitives/portal";
-import { Header } from "app/features/header";
 import { ToastProvider } from "./toasts";
+import { Div } from "@dishify/ui/src";
 
 export function Provider({
   children,
-  initialSession,
 }: {
   children: React.ReactNode;
-  initialSession: Session | null;
 }) {
   return (
     <SafeAreaProvider>
       <SolitoImageProvider>
-        {/* <AuthProvider initialSession={initialSession}> */}
         <TRPCProvider>
           <ThemeProvider>
             <ToastProvider>{children}</ToastProvider>
             <PortalHost />
           </ThemeProvider>
         </TRPCProvider>
-        {/* </AuthProvider> */}
       </SolitoImageProvider>
     </SafeAreaProvider>
   );
