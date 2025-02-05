e789f05c0ce0c0e87d3b4b05e8ede8e1c54dea68
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 06:28:57 -0500

    refactor(ui): replace Pressable with Button in OptionItem component

--- packages/ui/src/elements/autocomplete/options-list.tsx
+++ packages/ui/src/elements/autocomplete/options-list.tsx
@@ -4,6 +4,7 @@ import { Text } from "../../elements/text";
 import { cn } from "../../utils";
 import type { OptionItemProps, OptionsListProps } from "./types";
 import { Search } from "../../icons/search";
+import { Button } from "../button";
 
 export const OptionsList = React.memo(({ options, selectedIndex, onSelect }: OptionsListProps) => (
   <View className="w-full rounded-xl border border-sage-200 shadow-lg shadow-sage-500/5 p-1 pb-2 sm:px-2 animate-in fade-in-0 duration-300">
@@ -20,9 +21,10 @@ export const OptionsList = React.memo(({ options, selectedIndex, onSelect }: Opt
 ));
 
 export const OptionItem = React.memo(({ option, onPress, isSelected }: OptionItemProps) => (
-  <Pressable
+  <Button
+    variant="none"
     key={option}
-    onPress={() => onPress(option)}
+    onClick={() => onPress(option)}
     className={cn(
       "w-full flex-1 flex-row gap-2 sm:gap-0 items-center group web:cursor-default transition-colors duration-100 ease-in-out web:select-none rounded-lg py-1.5 native:py-2 px-2 sm:px-4 web:hover:bg-sage-100 web:outline-none",
       isSelected && "bg-sage-100",
@@ -36,5 +38,5 @@ export const OptionItem = React.memo(({ option, onPress, isSelected }: OptionIte
     >
       {option}
     </Text>
-  </Pressable>
+  </Button>
 ));
