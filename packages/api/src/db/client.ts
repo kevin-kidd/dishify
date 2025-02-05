a73e3682f2ec3a05eb1503020cbcaf1f65a54b45
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:36:35 -0500

    refactor(db): configure Drizzle ORM with combined user and recipe schemas

--- packages/api/src/db/client.ts
+++ packages/api/src/db/client.ts
@@ -1,5 +1,9 @@
 import { drizzle } from "drizzle-orm/d1";
+import * as userSchema from "./schema/user";
+import * as recipeSchema from "./schema/recipes";
+
+export const dbSchema = { ...userSchema, ...recipeSchema };
 
 export const createDb = (d1: D1Database) => {
-  return drizzle(d1);
+  return drizzle(d1, { schema: dbSchema });
 };
