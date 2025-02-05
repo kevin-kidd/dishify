0c89b51e37b64245d2ca1ceb44c92a8fd0df49b5
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 06:28:47 -0500

    fix(ui): replace onPress with onClick in Carousel navigation buttons

--- packages/ui/src/elements/carousel.tsx
+++ packages/ui/src/elements/carousel.tsx
@@ -194,7 +194,7 @@ const CarouselPrevious = React.forwardRef<
         className,
       )}
       disabled={!canScrollPrev}
-      onPress={scrollPrev}
+      onClick={scrollPrev}
       {...props}
     >
       <ArrowLeft className="h-4 w-4" />
@@ -223,7 +223,7 @@ const CarouselNext = React.forwardRef<
         className,
       )}
       disabled={!canScrollNext}
-      onPress={scrollNext}
+      onClick={scrollNext}
       {...props}
     >
       <ArrowRight className="h-4 w-4" />
