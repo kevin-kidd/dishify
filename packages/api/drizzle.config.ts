283b5fb45ab4d12dc887be96bc4a1d7194a1260a
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:36:50 -0500

    chore(db): update Drizzle configuration for local and D1 database environments

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,13 +1,34 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-  driver: "d1",
-  dbCredentials: {
-    wranglerConfigPath: "wrangler.toml",
-    dbName: "production",
-  },
-  verbose: false,
-  strict: true,
-} satisfies Config;
+/*
+ * NOTE: Workaround to make drizzle studio work with D1.
+ * https://kevinkipp.com/blog/going-full-stack-on-astro-with-cloudflare-d1-and-drizzle/
+ * Github discussion: https://github.com/drizzle-team/drizzle-orm/discussions/1545#discussioncomment-8115423
+ */
+export default process.env.DB_LOCAL_PATH
+  ? defineConfig({
+      schema: ["./src/db/schema/user.ts", "./src/db/schema/recipes.ts"],
+      out: "./migrations",
+      dialect: "sqlite",
+      dbCredentials: {
+        url: process.env.DB_LOCAL_PATH,
+      },
+    })
+  : defineConfig({
+      schema: ["./src/db/schema/user.ts", "./src/db/schema/recipes.ts"],
+      out: "./migrations",
+      driver: "d1-http",
+      dialect: "sqlite",
+      dbCredentials: {
+        // biome-ignore lint/style/noNonNullAssertion: <explanation>
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+        // biome-ignore lint/style/noNonNullAssertion: <explanation>
+        token: process.env.CLOUDFLARE_USER_API_TOKEN!,
+        databaseId:
+          process.env.NODE_ENV === "preview"
+            ? // biome-ignore lint/style/noNonNullAssertion: <explanation>
+              process.env.DB_PREVIEW_DATABASE_ID!
+            : // biome-ignore lint/style/noNonNullAssertion: <explanation>
+              process.env.DB_PROD_DATABASE_ID!,
+      },
+    });
