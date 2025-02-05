ec1272a92700f33863f0ffe507120dab4ea55d0b
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:38:27 -0500

    feat(tailwind): update content path and add custom animation

--- apps/next/tailwind.config.js
+++ apps/next/tailwind.config.js
@@ -2,11 +2,14 @@ const { theme } = require("@dishify/ui/src/theme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "../../packages/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "../../packages/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   important: "html",
   theme: {
     ...theme,
+  },
+  animations: {
+    pulse: "pulse 1s cubic-bezier(0.4, 0, 0.2, 1) infinite",
   },
   plugins: [require("tailwindcss-animate")],
 };
