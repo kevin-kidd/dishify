4fc727bc37920c6d9955a4119489784fe5cae30c
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 07:37:37 -0500

    feat(recipe): expose getRecipeBySlug procedure in recipe router

--- packages/api/src/routes/recipe/index.ts
+++ packages/api/src/routes/recipe/index.ts
@@ -1,7 +1,7 @@
 import { router } from "../../trpc";
 import { autocomplete } from "./autocomplete";
 import { generate } from "./generate";
-import { getRecipe } from "./get";
+import { getRecipe, getRecipeBySlug } from "./get";
 import { toggleFavorite } from "./toggle-favorite";
 import { recipeReactionsRouter } from "./reactions";
 
@@ -9,6 +9,7 @@ export const recipeRouter = router({
   generate,
   autocomplete,
   getRecipe,
+  getRecipeBySlug,
   toggleFavorite,
   reactions: recipeReactionsRouter,
 });
