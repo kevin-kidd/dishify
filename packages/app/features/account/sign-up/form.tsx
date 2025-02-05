56318e90b2534aa644d00d9cd1916fa1e47194d9
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 06:30:36 -0500

    refactor(app): replace onPress with onClick in SignUpForm Button

--- packages/app/features/account/sign-up/form.tsx
+++ packages/app/features/account/sign-up/form.tsx
@@ -181,7 +181,7 @@ export function SignUpForm() {
         )}
       />
       <Button
-        onPress={onSubmit}
+        onClick={onSubmit}
         aria-label="Submit"
         className="flex w-full h-10 items-center justify-center gap-2 rounded-md text-center transition ease-in-out"
       >
