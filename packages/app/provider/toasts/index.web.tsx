0fd9d2a6da000a023b52ff12787d10b5d43aeb71
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:31:15 -0500

    feat(ui): add close button to toast notifications

--- packages/app/provider/toasts/index.web.tsx
+++ packages/app/provider/toasts/index.web.tsx
@@ -1,13 +1,14 @@
 import { appColorSchemeAtom } from "app/atoms/theme";
 import { useAtomValue } from "jotai";
 import { Toaster } from "sonner";
+import React from "react";
 
 export function ToastProvider({ children }) {
   const appColorScheme = useAtomValue(appColorSchemeAtom);
   return (
     <>
       {children}
-      <Toaster position="bottom-right" theme={appColorScheme} />
+      <Toaster position="bottom-right" theme={appColorScheme} closeButton />
     </>
   );
 }
