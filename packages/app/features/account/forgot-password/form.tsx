10a66f7fb296e0701d6365a91f993d9427527e0b
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 01:36:06 -0500

    feat(account): add ForgotPasswordScreen and ForgotPasswordForm components

---
+++ packages/app/features/account/forgot-password/form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Text } from "react-native";
+import { Button, Form, FormInput, TextInput } from "@dishify/ui";
+import { toast } from "app/utils/toast";
+import { authClient } from "app/utils/auth/client";
+
+export function ForgotPasswordForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ForgotPasswordFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+  const onSubmit = handleSubmit(async (data) => {
+    const { error } = await authClient.forgetPassword({
+      email: data.email,
+      redirectTo: "/account/update-password",
+    });
+    if (error) {
+      toast.error("Error", {
+        description: error.message,
+      });
+    } else {
+      toast.success("Success", {
+        description: "You have successfully signed in!",
+      });
+    }
+  });
+  return (
+    <Form className="flex flex-col gap-y-4 mt-4 w-full">
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, name, value } }) => (
+          <FormInput error={errors.email?.message} label="Email" id={name}>
+            <TextInput
+              className="w-full"
+              inputMode="email"
+              textContentType="emailAddress"
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+              autoComplete="email"
+              autoCorrect={false}
+            />
+          </FormInput>
+        )}
+      />
+
+      <Button
+        onPress={onSubmit}
+        aria-label="Submit"
+        className="flex w-full h-10 items-center justify-center gap-2 rounded-md text-center transition ease-in-out"
+      >
+        <Text className="text-background">Reset Password</Text>
+      </Button>
+    </Form>
+  );
+}
+
+type ForgotPasswordFormValues = {
+  email: string;
+};
+
+const formSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+});
---
+++ packages/app/features/account/forgot-password/screen.tsx
@@ -0,0 +1,37 @@
+import { TextLink, P, Image, Main, Section, Text } from "@dishify/ui";
+import { ForgotPasswordForm } from "./form";
+import AccountLayout from "../layout";
+
+export function ForgotPasswordScreen() {
+  return (
+    <AccountLayout>
+      <Main className="flex flex-col min-h-screen items-center gap-0 justify-center max-w-xl w-full container mx-auto">
+        <Image
+          className="w-32 sm:w-40 2xl:w-48 mx-auto"
+          width={400}
+          height={200}
+          alt="Dishify"
+          src="/logo-large.png"
+          priority
+          contentFit="contain"
+          fill={false}
+        />
+        <Text className="mt-6 text-2xl font-bold 2xl:text-3xl text-center text-foreground">
+          Recover your account
+        </Text>
+        <P className="text-sm text-sage-700 2xl:text-base text-center py-0">
+          Don&apos;t have an account?{" "}
+          <TextLink
+            href="/account/sign-up"
+            className="font-normal text-sm 2xl:text-base text-sage-500 hover:text-sage-600 transition-colors"
+          >
+            Sign up
+          </TextLink>
+        </P>
+        <Section className="w-full flex flex-col gap-1 px-6 py-4 sm:px-0">
+          <ForgotPasswordForm />
+        </Section>
+      </Main>
+    </AccountLayout>
+  );
+}
