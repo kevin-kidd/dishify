309c0b8c0d45b576c17a3d3e61c5b8cfa5bec75b
Kevin Kidd <harrison.kevin@pm.me>
2025-02-06 00:44:50 -0500

    chore(nextjs): remove unnecessary newline in sentry client config

--- apps/next/sentry.client.config.ts
+++ apps/next/sentry.client.config.ts
@@ -6,7 +6,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
 
