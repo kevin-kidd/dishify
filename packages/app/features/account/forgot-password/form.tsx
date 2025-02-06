b8d133b2e4e3265f70ec46e54d35f44e8eac493a
Kevin Kidd <harrison.kevin@pm.me>
2025-02-05 23:24:42 -0500

    fix(account/forgot-password): update button event and screen styling adjustments

--- packages/app/features/account/forgot-password/form.tsx
+++ packages/app/features/account/forgot-password/form.tsx
@@ -59,7 +59,7 @@ export function ForgotPasswordForm() {
       />
 
       <Button
-        onClick={onSubmit}
+        onPress={onSubmit}
         aria-label="Submit"
         className="flex w-full h-10 items-center justify-center gap-2 rounded-md text-center transition ease-in-out"
       >
