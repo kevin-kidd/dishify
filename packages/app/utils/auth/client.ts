a31883754b0312d1a31ceec0070118bca50c4e74
Kevin Kidd <harrison.kevin@pm.me>
2025-02-06 19:23:17 -0500

    feat(auth): import env utility in client auth configuration

--- packages/app/utils/auth/client.ts
+++ packages/app/utils/auth/client.ts
@@ -1,6 +1,7 @@
 "use client";
 
 import { createAuthClient } from "better-auth/react";
+import { env } from "../env";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL,
