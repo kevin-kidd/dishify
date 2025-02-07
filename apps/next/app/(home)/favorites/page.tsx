3ea2129c16f77f17a5ad4a50e79e19a6bdadd069
Kevin Kidd <harrison.kevin@pm.me>
2025-02-06 20:17:16 -0500

    fix(favorites): await favorites query to ensure proper data fetching

--- apps/next/app/(home)/favorites/page.tsx
+++ apps/next/app/(home)/favorites/page.tsx
@@ -4,7 +4,7 @@ import { serverClient } from "utils/trpc";
 export default async function FavoritesPage() {
   try {
     // Prefetch favorites
-    serverClient.recipe.favorites.getFavorites.query();
+    await serverClient.recipe.favorites.getFavorites.query();
   } catch (error) {
     console.error(error);
   }
