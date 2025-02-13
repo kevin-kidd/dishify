6bfe6432a23c44067b9b1b6352e469837ba4a311
Kevin Kidd <harrison.kevin@pm.me>
2025-02-13 06:42:19 -0500

    feat(db): include marketplace schema in database client

--- packages/api/src/db/client.ts
+++ packages/api/src/db/client.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/d1";
 import * as userSchema from "./schema/user";
 import * as recipeSchema from "./schema/recipes";
+import * as marketplaceSchema from "./schema/marketplace";
 
-export const dbSchema = { ...userSchema, ...recipeSchema };
+export const dbSchema = { ...userSchema, ...recipeSchema, ...marketplaceSchema };
 
 export const createDb = (d1: D1Database) => {
   return drizzle(d1, { schema: dbSchema });
