21b9dcbfc4e3fd61ad27544be260f80bb367b9b4
Kevin Kidd <harrison.kevin@pm.me>
2025-02-13 06:43:13 -0500

    feat(router): integrate marketplace router into app router

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,12 +1,14 @@
 import { authRouter } from "./routes/auth";
 import { recipeRouter } from "./routes/recipe";
 import { userRouter } from "./routes/user";
+import { marketplaceRouter } from "./routes/marketplace";
 import { router } from "./trpc";
 
 export const appRouter = router({
   user: userRouter,
   auth: authRouter,
   recipe: recipeRouter,
+  marketplace: marketplaceRouter,
 });
 
 export type AppRouter = typeof appRouter;
