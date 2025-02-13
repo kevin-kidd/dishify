dcdd529843b86f8968c9cc7527a732bc22b70aa6
Kevin Kidd <harrison.kevin@pm.me>
2025-02-13 09:05:09 -0500

    fix(api): simplify autocomplete query to include exact matches

--- packages/api/src/routes/recipe/autocomplete.ts
+++ packages/api/src/routes/recipe/autocomplete.ts
@@ -38,7 +38,7 @@ export const autocomplete = publicProcedure
     const results = await db
       .select({ name: table.name })
       .from(table)
-      .where(and(like(table.name, `${query}%`), sql`LOWER(${table.name}) != LOWER(${query})`))
+      .where(like(table.name, `${query}%`))
       .limit(5);
     return results.map((result) => result.name);
   });
