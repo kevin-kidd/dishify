da06c92b8e4b0e9a258c933db9cf50ff4dd9d554
Kevin Kidd <harrison.kevin@pm.me>
2025-02-14 02:02:09 -0500

    feat(auth): add isSignedIn state to useAuth hook

--- packages/app/utils/hooks/use-auth.ts
+++ packages/app/utils/hooks/use-auth.ts
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { authClient } from "../auth/client";
 
 export const useAuth = () => {
   const { useSession, updateUser, signIn, signOut, signUp } = authClient;
+  const [isSignedIn, setIsSignedIn] = useState<"fetching" | "signed-in" | "signed-out">("fetching");
+  const session = useSession();
 
-  return { useSession, updateUser, signIn, signOut, signUp };
+  useEffect(() => {
+    if (session.isPending) return;
+    if (session.data?.user) {
+      setIsSignedIn("signed-in");
+    } else {
+      setIsSignedIn("signed-out");
+    }
+  }, [session]);
+  return {
+    useSession,
+    updateUser,
+    signIn,
+    signOut,
+    signUp,
+    isSignedIn,
+    session,
+    isPending: session.isPending,
+  };
 };
