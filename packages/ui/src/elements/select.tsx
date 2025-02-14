a1377c8a1ebbdee6e7eca277a4f40515c6ea2224
Kevin Kidd <harrison.kevin@pm.me>
2025-02-14 02:00:38 -0500

    refactor(ui): enhance select component with improved styling and type definitions

--- packages/ui/src/elements/select.tsx
+++ packages/ui/src/elements/select.tsx
@@ -1,13 +1,12 @@
-"use client";
-
 import * as SelectPrimitive from "@rn-primitives/select";
 import * as React from "react";
 import { Platform, StyleSheet, View } from "react-native";
 import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
-import { cn } from "../utils";
-import { ChevronDown } from "../icons/chevron-down";
 import { Check } from "../icons/check";
+import { ChevronDown } from "../icons/chevron-down";
 import { ChevronUp } from "../icons/chevron-up";
+import { cn } from "../utils";
+import { useAugmentedRef } from "@rn-primitives/hooks";
 
 type Option = SelectPrimitive.Option;
 
@@ -20,34 +19,33 @@ const SelectValue = SelectPrimitive.Value;
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Trigger
-    ref={ref}
-    style={{
-      $$css: true,
-      className: cn(
-        "flex-1 flex-row h-10 web:select-none native:h-12 items-center text-sm justify-between rounded-md border border-input bg-background px-3 py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1",
+>(({ className, children, ...props }, ref) => {
+  const triggerRef = useAugmentedRef({ ref });
+
+  return (
+    <SelectPrimitive.Trigger
+      ref={triggerRef}
+      className={cn(
+        "flex flex-row h-10 native:h-12 items-center text-xs sm:text-sm justify-between rounded-md border border-input bg-background py-1.5 px-2.5 sm:px-3 sm:py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1 break-all",
         props.disabled && "web:cursor-not-allowed opacity-50",
         className,
-      ),
-    }}
-    {...props}
-  >
-    <>
-      {children}
+      )}
+      onPress={() => {
+        triggerRef?.current?.open();
+      }}
+      {...props}
+    >
+      {children as React.ReactNode}
       <ChevronDown size={16} aria-hidden={true} className="text-foreground opacity-50" />
-    </>
-  </SelectPrimitive.Trigger>
-));
+    </SelectPrimitive.Trigger>
+  );
+});
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
 
 /**
  * Platform: WEB ONLY
  */
-const SelectScrollUpButton = ({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>) => {
+const SelectScrollUpButton = ({ className, ...props }: SelectPrimitive.ScrollUpButtonProps) => {
   if (Platform.OS !== "web") {
     return null;
   }
@@ -64,10 +62,7 @@ const SelectScrollUpButton = ({
 /**
  * Platform: WEB ONLY
  */
-const SelectScrollDownButton = ({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>) => {
+const SelectScrollDownButton = ({ className, ...props }: SelectPrimitive.ScrollDownButtonProps) => {
   if (Platform.OS !== "web") {
     return null;
   }
@@ -81,35 +76,30 @@ const SelectScrollDownButton = ({
   );
 };
 
-const SelectContent = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content> & { portalHost?: string }
->(({ className, children, position = "popper", portalHost, ...props }, ref) => {
+const SelectContent: React.ForwardRefExoticComponent<
+  SelectPrimitive.ContentProps & {
+    portalHost?: string;
+  } & React.RefAttributes<SelectPrimitive.ContentRef>
+> = React.forwardRef(({ className, children, position = "popper", portalHost, ...props }, ref) => {
   const { open } = SelectPrimitive.useRootContext();
 
   return (
     <SelectPrimitive.Portal
       hostName={portalHost}
-      container={document.getElementById("theme-provider")}
+      container={Platform.OS === "web" ? document.getElementById("theme-provider") : undefined}
     >
       <SelectPrimitive.Overlay style={Platform.OS !== "web" ? StyleSheet.absoluteFill : undefined}>
-        <Animated.View
-          entering={Platform.OS !== "web" ? FadeIn : undefined}
-          exiting={Platform.OS !== "web" ? FadeOut : undefined}
-        >
+        <Animated.View className="z-50" entering={FadeIn} exiting={FadeOut}>
           <SelectPrimitive.Content
             ref={ref}
-            style={{
-              $$css: true,
-              className: cn(
-                "select-content-child relative z-50 max-h-96 min-w-[8rem] rounded-md border border-border bg-popover shadow-md shadow-foreground/10 py-2 px-1 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 & data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:mt-2 data-[side=left]:ml-1 data-[side=right]:mr-1 data-[side=top]:mb-2",
-                position === "popper" && "select-content-popper",
-                open
-                  ? "web:zoom-in-95 web:animate-in web:fade-in-0"
-                  : "web:zoom-out-95 web:animate-out web:fade-out-0",
-                className,
-              ),
-            }}
+            className={cn(
+              "select-content-child relative z-50 max-h-96 min-w-[8rem] rounded-md border border-border bg-popover shadow-md shadow-foreground/10 py-2 px-1 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 & data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:mt-2 data-[side=left]:ml-1 data-[side=right]:mr-1 data-[side=top]:mb-2",
+              position === "popper" && "select-content-popper",
+              open
+                ? "web:zoom-in-95 web:animate-in web:fade-in-0"
+                : "web:zoom-out-95 web:animate-out web:fade-out-0",
+              className,
+            )}
             position={position}
             {...props}
           >
@@ -118,7 +108,7 @@ const SelectContent = React.forwardRef<
               className={cn(
                 "p-1",
                 position === "popper" &&
-                  "h-[var(--radix-select-trigger-height)] w-full native:min-w-[var(--radix-select-trigger-width)]",
+                  "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
               )}
             >
               {children}
@@ -132,65 +122,49 @@ const SelectContent = React.forwardRef<
 });
 SelectContent.displayName = SelectPrimitive.Content.displayName;
 
-const SelectLabel = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Label>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Label
-    ref={ref}
-    style={{
-      $$css: true,
-      className: cn(
+const SelectLabel = React.forwardRef<SelectPrimitive.LabelRef, SelectPrimitive.LabelProps>(
+  ({ className, ...props }, ref) => (
+    <SelectPrimitive.Label
+      ref={ref}
+      className={cn(
         "py-1.5 native:pb-2 pl-8 native:pl-10 pr-2 text-popover-foreground text-sm native:text-base font-semibold",
         className,
-      ),
-    }}
-    {...props}
-  />
-));
+      )}
+      {...props}
+    />
+  ),
+);
 SelectLabel.displayName = SelectPrimitive.Label.displayName;
 
-const SelectItem = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Item
-    ref={ref}
-    className={cn(
-      "relative web:group flex flex-row w-full web:cursor-default web:select-none items-center rounded-sm py-1.5 native:py-2 pl-8 native:pl-10 pr-2 web:hover:bg-sage-100 active:bg-sage-100 web:outline-none web:focus:bg-sage-100",
-      props.disabled && "web:pointer-events-none opacity-50",
-      className,
-    )}
-    {...props}
-  >
-    <View className="absolute left-2 native:left-3.5 flex h-3.5 native:pt-px w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check size={16} strokeWidth={3} className="text-popover-foreground" />
-      </SelectPrimitive.ItemIndicator>
-    </View>
-    <SelectPrimitive.ItemText
-      style={{
-        $$css: true,
-        className: cn(
-          "text-sm native:text-lg text-popover-foreground native:text-base web:group-focus:text-accent-foreground",
-          className,
-        ),
-      }}
-    />
-  </SelectPrimitive.Item>
-));
+const SelectItem = React.forwardRef<SelectPrimitive.ItemRef, SelectPrimitive.ItemProps>(
+  ({ className, children, ...props }, ref) => (
+    <SelectPrimitive.Item
+      ref={ref}
+      className={cn(
+        "relative web:group flex flex-row w-full web:cursor-default web:select-none items-center rounded-sm py-1.5 native:py-2 pl-8 native:pl-10 pr-2 web:hover:bg-accent/50 active:bg-accent web:outline-none web:focus:bg-accent",
+        props.disabled && "web:pointer-events-none opacity-50",
+        className,
+      )}
+      {...props}
+    >
+      <View className="absolute left-2 native:left-3.5 flex h-3.5 native:pt-px w-3.5 items-center justify-center">
+        <SelectPrimitive.ItemIndicator>
+          <Check size={16} strokeWidth={3} className="text-popover-foreground" />
+        </SelectPrimitive.ItemIndicator>
+      </View>
+      <SelectPrimitive.ItemText className="sm:text-sm text-xs text-popover-foreground native:text-base web:group-focus:text-accent-foreground" />
+    </SelectPrimitive.Item>
+  ),
+);
 SelectItem.displayName = SelectPrimitive.Item.displayName;
 
 const SelectSeparator = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Separator>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
+  SelectPrimitive.SeparatorRef,
+  SelectPrimitive.SeparatorProps
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
     ref={ref}
-    style={{
-      $$css: true,
-      className: cn("-mx-1 my-1 h-px bg-muted", className),
-    }}
+    className={cn("-mx-1 my-1 h-px bg-muted", className)}
     {...props}
   />
 ));
