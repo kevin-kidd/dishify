50dfb6504d99047383e5acfe751904a980348e59
Kevin Kidd <harrison.kevin@pm.me>
2025-02-27 21:34:20 -0500

    feat(home): expand HomeScreen with comprehensive recipe sections

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,7 +1,20 @@
 "use client";
 
 import { TrendingSection } from "./trending";
+import { CategoriesSection } from "./categories";
+import { FeaturedRecipeSection } from "./featured-recipe";
+import { CookingTipsSection } from "./cooking-tips";
+import { SeasonalSection } from "./seasonal";
+import { Div } from "@dishify/ui/src";
 
 export function HomeScreen() {
-  return <TrendingSection />;
+  return (
+    <Div className="flex flex-col w-full overflow-x-hidden">
+      <TrendingSection />
+      <FeaturedRecipeSection />
+      <CategoriesSection />
+      <SeasonalSection />
+      <CookingTipsSection />
+    </Div>
+  );
 }
