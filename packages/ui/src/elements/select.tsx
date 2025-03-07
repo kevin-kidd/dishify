015aca5dc4936fbf9747bdc8c2ce6dde9a63931b
Kevin Kidd <harrison.kevin@pm.me>
2025-03-06 19:03:31 -0500

    refactor(ui): optimize select component imports and styling

--- packages/ui/src/elements/select.tsx
+++ packages/ui/src/elements/select.tsx
@@ -1,14 +1,11 @@
-"use client";
-
 import * as SelectPrimitive from "@rn-primitives/select";
 import * as React from "react";
 import { Platform, StyleSheet, View } from "react-native";
 import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
-import { Check } from "../icons/check";
-import { ChevronDown } from "../icons/chevron-down";
-import { ChevronUp } from "../icons/chevron-up";
+import { Check } from "@dishify/ui/src/icons/check";
+import { ChevronDown } from "@dishify/ui/src/icons/chevron-down";
+import { ChevronUp } from "@dishify/ui/src/icons/chevron-up";
 import { cn } from "../utils";
-import { useAugmentedRef } from "@rn-primitives/hooks";
 
 type Option = SelectPrimitive.Option;
 
@@ -18,30 +15,22 @@ const SelectGroup = SelectPrimitive.Group;
 
 const SelectValue = SelectPrimitive.Value;
 
-const SelectTrigger = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => {
-  const triggerRef = useAugmentedRef({ ref });
-
-  return (
+const SelectTrigger = React.forwardRef<SelectPrimitive.TriggerRef, SelectPrimitive.TriggerProps>(
+  ({ className, children, ...props }, ref) => (
     <SelectPrimitive.Trigger
-      ref={triggerRef}
+      ref={ref}
       className={cn(
-        "flex flex-row h-10 native:h-12 items-center text-xs sm:text-sm justify-between rounded-md border border-input bg-background py-1.5 px-2.5 sm:px-3 sm:py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1 break-all",
+        "flex flex-row h-10 native:h-12 items-center text-sm justify-between rounded-md border border-input bg-background px-3 py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1",
         props.disabled && "web:cursor-not-allowed opacity-50",
         className,
       )}
-      onPress={() => {
-        triggerRef?.current?.open();
-      }}
       {...props}
     >
-      {children as React.ReactNode}
+      <>{children}</>
       <ChevronDown size={16} aria-hidden={true} className="text-foreground opacity-50" />
     </SelectPrimitive.Trigger>
-  );
-});
+  ),
+);
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
 
 /**
@@ -78,25 +67,25 @@ const SelectScrollDownButton = ({ className, ...props }: SelectPrimitive.ScrollD
   );
 };
 
-const SelectContent: React.ForwardRefExoticComponent<
-  SelectPrimitive.ContentProps & {
-    portalHost?: string;
-  } & React.RefAttributes<SelectPrimitive.ContentRef>
-> = React.forwardRef(({ className, children, position = "popper", portalHost, ...props }, ref) => {
+const SelectContent = React.forwardRef<
+  SelectPrimitive.ContentRef,
+  SelectPrimitive.ContentProps & { portalHost?: string }
+>(({ className, children, position = "popper", portalHost, ...props }, ref) => {
   const { open } = SelectPrimitive.useRootContext();
 
   return (
     <SelectPrimitive.Portal
       hostName={portalHost}
-      container={Platform.OS === "web" ? document.getElementById("theme-provider") : undefined}
+      container={document.getElementById("theme-provider")}
     >
       <SelectPrimitive.Overlay style={Platform.OS !== "web" ? StyleSheet.absoluteFill : undefined}>
         <Animated.View className="z-50" entering={FadeIn} exiting={FadeOut}>
           <SelectPrimitive.Content
             ref={ref}
             className={cn(
-              "select-content-child relative z-50 max-h-96 min-w-[8rem] rounded-md border border-border bg-popover shadow-md shadow-foreground/10 py-2 px-1 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 & data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:mt-2 data-[side=left]:ml-1 data-[side=right]:mr-1 data-[side=top]:mb-2",
-              position === "popper" && "select-content-popper",
+              "relative z-50 max-h-96 min-w-[8rem] rounded-md border border-border bg-popover shadow-md shadow-foreground/10 py-2 px-1 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+              position === "popper" &&
+                "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
               open
                 ? "web:zoom-in-95 web:animate-in web:fade-in-0"
                 : "web:zoom-out-95 web:animate-out web:fade-out-0",
@@ -154,7 +143,7 @@ const SelectItem = React.forwardRef<SelectPrimitive.ItemRef, SelectPrimitive.Ite
           <Check size={16} strokeWidth={3} className="text-popover-foreground" />
         </SelectPrimitive.ItemIndicator>
       </View>
-      <SelectPrimitive.ItemText className="sm:text-sm text-xs text-popover-foreground native:text-base web:group-focus:text-accent-foreground" />
+      <SelectPrimitive.ItemText className="text-sm native:text-lg text-popover-foreground native:text-base web:group-focus:text-accent-foreground" />
     </SelectPrimitive.Item>
   ),
 );
