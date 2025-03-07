81ebbabf25932fb1c286c15386910463c17868b3
Kevin Kidd <harrison.kevin@pm.me>
2025-03-06 19:05:30 -0500

    feat(database): add image URL, description, and category columns to recipe details

---
+++ packages/api/migrations/0002_careless_chronomancer.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `english_recipe_details` ADD `image_url` text;--> statement-breakpoint
+ALTER TABLE `english_recipe_details` ADD `description` text;--> statement-breakpoint
+ALTER TABLE `english_recipe_details` ADD `category` text;
