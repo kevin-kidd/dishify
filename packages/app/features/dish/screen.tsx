066b0616ff613514cef0367ce012ec0a07139c4a
Kevin Kidd <harrison.kevin@pm.me>
2025-03-06 20:38:50 -0500

    feat(dish): create DishScreen component with RecipeCard

---
+++ packages/app/features/dish/screen.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Section } from "@dishify/ui/src";
+import RecipeCard from "./recipe-card";
+
+export default function DishScreen() {
+  return (
+    <Section>
+      <RecipeCard />
+    </Section>
+  );
+}
