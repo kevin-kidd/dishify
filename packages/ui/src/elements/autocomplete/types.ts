8a5b9d92ce65366957a4ca4b8a5421c0ce0d1565
Kevin Kidd <harrison.kevin@pm.me>
2025-04-16 21:33:46 -0400

    refactor(autocomplete): add optional setIsOptionClicking prop for enhanced option selection handling

--- packages/ui/src/elements/autocomplete/types.ts
+++ packages/ui/src/elements/autocomplete/types.ts
@@ -15,6 +15,7 @@ export interface OptionsListProps {
   options: string[];
   selectedIndex: number;
   onSelect: (option: string) => void;
+  setIsOptionClicking?: (val: boolean) => void;
 }
 
 export interface OptionItemProps {
